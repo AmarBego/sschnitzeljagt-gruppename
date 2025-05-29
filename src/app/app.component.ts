9ae4448e0e4c00e475f1d3f47e1021bf9bef3132
AmarBego <begovicamar@proton.me>
2025-05-29 16:20:09 +0200

    fixed trash and iconing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trashBin, home, search, person, settings } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonIcon],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({ trashBin, home, search, person, settings });
+  }
 }
